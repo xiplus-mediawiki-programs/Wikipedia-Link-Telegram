f7daab812be6d7e22806e348482b9ac70a4b4534
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-06-28 21:47:21 +0800

    update config variable

--- install/setWebhook.php
+++ install/setWebhook.php
@@ -1,5 +1,5 @@
 <?php
 require(__DIR__."/../config/config.default.php");
-$commend = 'curl https://api.telegram.org/bot'.$cfg["token"].'/setWebhook -d "url='.urlencode($cfg["webhook"]).'"';
+$commend = 'curl https://api.telegram.org/bot'.$C["token"].'/setWebhook -d "url='.urlencode($C["webhook"]).'"';
 echo $commend;
 system($commend);
