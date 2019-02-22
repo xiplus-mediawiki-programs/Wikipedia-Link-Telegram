0834276afb17c7226fcad1bf1135e26434164a1e
xiplus <huangxuanyuxiplus@gmail.com>
2019-02-22 22:13:50 +0800

    setWebhook: Add delete, get

--- install/setWebhook.php
+++ install/setWebhook.php
@@ -1,5 +1,24 @@
 <?php
-require(__DIR__."/../config/config.default.php");
-$commend = 'curl https://api.telegram.org/bot'.$C["token"].'/setWebhook -d "url='.urlencode($C["webhook"]).'&max_connections='.$C["max_connections"].'"';
+require __DIR__ . "/../config/config.default.php";
+
+$action = $argv[1] ?? "get";
+
+switch ($action) {
+	case 'set':
+		$commend = 'curl https://api.telegram.org/bot' . $C["token"] . '/setWebhook -d "url=' . urlencode($C["webhook"]) . '&max_connections=' . $C["max_connections"] . '"';
+		break;
+
+	case 'delete':
+		$commend = 'curl https://api.telegram.org/bot' . $C["token"] . '/deleteWebhook';
+		break;
+
+	case 'get':
+		$commend = 'curl https://api.telegram.org/bot' . $C["token"] . '/getWebhookinfo';
+		break;
+
+	default:
+		exit("Unknown action.");
+}
+
 echo $commend;
 system($commend);
