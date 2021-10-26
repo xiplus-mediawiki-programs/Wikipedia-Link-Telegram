cb69a4240b0f3246d220cbef7c39b2bac1991618
xiplus <huangxuanyuxiplus@gmail.com>
2021-10-26 10:15:05 +0800

    Lint

--- config/config.default.php
+++ config/config.default.php
@@ -10,11 +10,11 @@ $C['unusedlimit'] = 86400 * 7;
 $C['parseconfigretry'] = 3;
 $C['notreplyhelplist'] = [];
 $C['operator'] = [];
-$C['specialrule'] = function($chat_id, &$articlepath, &$page, &$no404) {};
+$C['specialrule'] = function ($chat_id, &$articlepath, &$page, &$no404) {};
 
 $C['logkeep'] = 86400 * 30;
 
-$C['module']['mediawikiurlencode'] = __DIR__.'/function/Mediawiki-urlencode/mediawikiurlencode.php';
+$C['module']['mediawikiurlencode'] = __DIR__ . '/function/Mediawiki-urlencode/mediawikiurlencode.php';
 
 $C['DBhost'] = 'localhost';
 $C['DBname'] = 'WikipediaLinkBot';
@@ -22,6 +22,6 @@ $C['DBuser'] = 'root';
 $C['DBpass'] = '';
 $C['DBTBprefix'] = '';
 
-@include(__DIR__.'/config.php');
+@include __DIR__ . '/config.php';
 
-$G['db'] = new PDO ('mysql:host='.$C['DBhost'].';dbname='.$C['DBname'].';charset=utf8mb4', $C['DBuser'], $C['DBpass']);
+$G['db'] = new PDO('mysql:host=' . $C['DBhost'] . ';dbname=' . $C['DBname'] . ';charset=utf8mb4', $C['DBuser'], $C['DBpass']);
